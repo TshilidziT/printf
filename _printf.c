673b27a2a64e60f6648b1d3741954c7946c75c60
Miss-Siba <sibaquma@gmail.com>
2023-10-14 11:20:04 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -4,11 +4,11 @@
 /**
  * _printf - function that produces output accoring to a format.
  * @format: character string
- * Return: count
+ * Return: i
  */
 int _printf(const char *format, ...)
 {
-	int i, count = 0;
+	int i;
 	va_list args;
 
 	va_start(args, format);
@@ -20,22 +20,23 @@ int _printf(const char *format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					count += fprintf(stdout, "%c", va_arg(args, int));
+					count += printf("%c", va_arg(args, int));
 					break;
 				case 's':
-					count += fprintf(stdout, "%s", va_arg(args, char *));
+					count += printf("%s", va_arg(args, char *));
 					break;
 
 				default:
-					putchar('%');
-					putchar(format[i]);
-					count += 2;
+					printf("%%")
 					break;
 			}
 
-			format++;
-		}
+			else
+			{
+
+			printf("%c"format[i]);
+			}
 	}
 	va_end(args);
-	return (count);
+	return (i);
 }
