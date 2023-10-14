6b6e8a248b42206097777d9b217e4589ae259487
Miss-Siba <sibaquma@gmail.com>
2023-10-14 07:41:35 -0700

    task 0

---
+++ test/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *format = "ALX is %s";
+
+	printf(format, "great");
+	printf("\n");
+
+	return (0);
+
+}
