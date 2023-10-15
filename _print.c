3b40f8cd5c8bea20c5663022be1be7faab754e13
Miss-Siba <sibaquma@gmail.com>
2023-10-15 06:11:21 -0700

    task 0

---
+++ _print.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * _printf - function that produces output accoring to a format.
+ * @format: character string
+ * Return: count
+ */
+
+int _printf(const char *format, ...)
+{
+	int i, count = 0;
+	char c;
+	char *str;
+	va_list arg;
+
+	va_start(arg, format);
+
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+				break;
+
+			switch (format[i])
+			{
+				case 'c':
+					c = va_arg(arg, int);
+					write(1, &c, 1);
+					count++;
+					break;
+				case 's':
+					str = va_arg(arg, char *);
+					if (str != NULL)
+					{
+					write(1, str, strlen(str));
+					count++;
+					}
+					break;
+				case '%':
+					write(1, &format[i], 1);
+					count++;
+					break;
+
+			}
+		}
+
+		else
+		{
+		write(1, &format[i], 1);
+			count++;
+		}
+	}
+
+	va_end(arg);
+
+	return (count);
+}
