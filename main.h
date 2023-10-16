5bbad0e46a768e6398bcd8d511b8ac5a9b9d4b3a
TshilidziT <Tshilidzibytee@gmail.com>
2023-10-16 13:22:25 -0700

    task1

--- main.h
+++ main.h
@@ -5,12 +5,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
 int handleChar(int c);
 int handleString(char *str);
 int handlePercent(void);
 int handleDefault(const char *format, int i);
+char *int_to_str(int num);
+void my_printf(char *format, ...);
+void print_decimal(char specifier, int number);
+void print_str(char *str);
+
 
 #endif
