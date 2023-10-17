41d3672d5f8d65344f2a5d0cac600a424d32071c
Miss-Siba <sibaquma@gmail.com>
2023-10-17 03:41:32 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int handleString(char *str);
+int handlePercent(void);
+int handleChar(int c);
+int handleDefault(const char *format, int i);
+char *int_to_str(int number);
+void print_str(char *str);
+void print_decimal(char specifier, int number);
+char my_printf(const char *format, ...);
+
+#endif
